5f0d89f7a4bed2df26a9cc212580e2ace1490323
frankiefu <ffu@google.com>
2016-02-17 18:49:15 -0800

    build script for vulcanize templates, patterns, docs and the main page

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+
+vulcanize --inline-css --inline-scripts ../app-layout/patterns/expand-card/index.html > patterns/expand-card/index.html
+vulcanize --inline-css --inline-scripts ../app-layout/patterns/transform-navigation/index.html > patterns/transform-navigation/index.html
+
+vulcanize --inline-css --inline-scripts ../app-layout/templates/publishing/index.html > templates/publishing/index.html
+vulcanize --inline-css --inline-scripts ../app-layout/templates/pesto/index.html > templates/pesto/index.html
+vulcanize --inline-css --inline-scripts ../app-layout/templates/shrine/index.html > templates/shrine/index.html
+
+vulcanize --inline-css --inline-scripts ../app-layout/docs.html > docs.html
+vulcanize --inline-css --inline-scripts ../app-layout/index.html > index.html
