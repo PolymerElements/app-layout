c703ba6cd980744ff606680c0027d83ccdf2d668
Keanu Lee <keanulee@google.com>
2016-02-17 19:13:49 -0800

    Update build script and demos

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
+cd ../app-layout
+git checkout master
+git pull
+cd -
 
 vulcanize --inline-css --inline-scripts ../app-layout/patterns/expand-card/index.html > patterns/expand-card/index.html
 vulcanize --inline-css --inline-scripts ../app-layout/patterns/transform-navigation/index.html > patterns/transform-navigation/index.html
@@ -10,3 +14,10 @@ vulcanize --inline-css --inline-scripts ../app-layout/templates/shrine/index.htm
 
 vulcanize --inline-css --inline-scripts ../app-layout/docs.html > docs.html
 vulcanize --inline-css --inline-scripts ../app-layout/index.html > index.html
+
+cd ../app-layout
+git checkout sw
+git pull
+cd -
+
+vulcanize --inline-css --inline-scripts ../app-layout/templates/pesto/index.html > templates-sw/pesto/index.html
