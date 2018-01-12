ac84d80452ac82413cd91ad353dca3b8bd1f0258
Fred K. Schott <fschott@google.com>
2018-01-12 13:13:52 -0500

    auto-converted by polymer-modulizer

---
+++ app-scroll-effects/test/x-container.js
@@ -0,0 +1,93 @@
+import '../../../polymer/polymer.js';
+import { AppScrollEffectsBehavior } from '../app-scroll-effects-behavior.js';
+import { Polymer as Polymer$0 } from '../../../polymer/lib/legacy/polymer-fn.js';
+
+/**
+@license
+Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+Polymer$0({
+  _template: Polymer.html`
+    <style>
+      :host {
+        position: relative;
+        display: block;
+      }
+
+      #background,
+      #backgroundFrontLayer,
+      #backgroundRearLayer {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+      }
+
+      #backgroundFrontLayer {
+        bottom: -200px;
+      }
+
+      #mainTitle, #condensedTitle {
+        display: block;
+        overflow: hidden;
+        margin: 0;
+        padding: 0;
+        position: absolute;
+      }
+
+      #mainTitle {
+        height: 18px;
+        top: 0;
+        left: 0;
+      }
+
+      #condensedTitle {
+        height: 37px;
+        top: 100px;
+        left: 100px;
+      }
+
+    </style>
+
+    <div id="background"></div>
+    <div id="backgroundFrontLayer"></div>
+    <div id="backgroundRearLayer"></div>
+    <h4 id="mainTitle">Title</h4>
+    <h1 id="condensedTitle">Condensed title</h1>
+`,
+
+  is: 'x-container',
+
+  behaviors: [
+    AppScrollEffectsBehavior
+  ],
+
+  properties: {
+    shadow: {
+      type: Boolean,
+      reflectToAttribute: true
+    }
+  },
+
+  observers: [
+    '_xScrollEffectChanged(effects)'
+  ],
+
+  _getDOMRef: function(id) {
+    return this.$[id] || null;
+  },
+
+  _updateScrollState: function(scrollTop) {
+    this._runEffects(scrollTop / this.offsetHeight, scrollTop);
+  },
+
+  _xScrollEffectChanged: function() {
+    this._updateScrollState(this._scrollTop);
+  }
+});
---
+++ templates/getting-started/x-app.js
@@ -0,0 +1,54 @@
+import { Polymer as Polymer$0 } from '../../../polymer/lib/legacy/polymer-fn.js';
+
+/**
+@license
+Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+Polymer$0({
+  _template: Polymer.html`
+    <style>
+
+      app-header {
+        background-color: #00897B;
+        color: #fff;
+      }
+
+      paper-icon-button {
+        --paper-icon-button-ink-color: white;
+      }
+
+      app-drawer-layout:not([narrow]) [drawer-toggle] {
+        display: none;
+      }
+
+    </style>
+
+    <app-drawer-layout>
+
+      <app-drawer slot="drawer">
+        <app-toolbar>Getting Started</app-toolbar>
+      </app-drawer>
+
+      <app-header-layout>
+
+        <app-header slot="header" reveals="" effects="waterfall">
+          <app-toolbar>
+            <paper-icon-button icon="menu" drawer-toggle=""></paper-icon-button>
+            <div main-title="">Title</div>
+          </app-toolbar>
+        </app-header>
+
+        <sample-content size="100"></sample-content>
+
+      </app-header-layout>
+
+    </app-drawer-layout>
+`,
+
+  is: 'x-app'
+});
